be3d80a98696712c41c8992d47c88cfbdbb1bafa
jalxby <jalxby@gmail.com>
2024-04-04 22:19:11 +0200

    reexport fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import './styles/index.scss'
 export * from './ui/index'
+export * from './ui/text-field/model/text-field-types'
 export { clsx } from 'clsx'
--- src/ui/text-field/index.ts
+++ src/ui/text-field/index.ts
@@ -1,2 +1,1 @@
 export { TextField } from './ui/text-field'
-export * from './model/text-field-types'
